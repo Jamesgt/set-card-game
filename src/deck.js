8f2d1202ee5583819801e49bb05454b029a67a96
Tamas Gemes <tamas.gemes@gmail.com>
2021-05-21 23:06:16 +0200

    feat: layout

--- src/deck.js
+++ src/deck.js
@@ -16,14 +16,34 @@ class Card {
 class Deck {
 
   constructor() {
-    this.cards = Array.from({ length: 81 }, (_, i) => {
-      return new Card(
-        1 + i % 3,
-        ['diamond', 'squiggle', 'oval'][Math.floor(i / 3) % 3],
-        ['empty', 'half', 'full'][Math.floor(i / 9) % 3],
-        ['red', 'green', 'blue'][Math.floor(i / 27) % 3],
+    this.cards = _.shuffle(
+      Array.from(
+        { length: 81 },
+        (_, i) => {
+          return new Card(
+            1 + i % 3,
+            ['diamond', 'squiggle', 'oval'][Math.floor(i / 3) % 3],
+            ['empty', 'half', 'full'][Math.floor(i / 9) % 3],
+            ['red', 'green', 'blue'][Math.floor(i / 27) % 3],
+          )
+        }
       )
-    })
+    )
+    /** @type {Card[]} */
+    this.playing = []
+
+    this.draw(12)
+  }
+
+  /**
+   * Draw cards
+   * @param {number} n=3 The number of cards to draw, default is 3.
+   */
+  draw(n = 3) {
+    // this.playing = [...this.playing, ...this.cards.splice(0, n)] would be much more readable, but it is slower
+    this.playing.push.apply(this.playing, this.cards.splice(0, n))
   }
 
 }
+
+const deck = new Deck()
